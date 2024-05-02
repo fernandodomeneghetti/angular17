761fb6f90bed9df76dd2550226350a4bb36e4028
Fernando Miranda Domeneghetti <fernando.domeneghetti@totvs.com.br>
2024-05-02 13:23:40 -0300

    feat: create crud page usigin firestore

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { HomeComponent } from './components/home/home.component';
+import { CrudComponent } from './pages/crud/crud.component';
 
 const routes: Routes = [
   { path: '', component: LoginComponent },
   { path: 'login', component: LoginComponent },
-  { path: 'home', component: HomeComponent }
+  { path: 'home', component: HomeComponent },
+  { path: 'crud', component: CrudComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,10 +9,20 @@ import { LoginComponent } from './pages/login/login.component';
 import { FormsModule } from '@angular/forms';
 import { HomeComponent } from './components/home/home.component';
 import { MenuComponent } from './components/menu/menu.component';
+import { CrudComponent } from './pages/crud/crud.component';
 
 // Angular Material
 import { MatIconModule } from '@angular/material/icon';
 import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { environment } from '../environments/environment.development';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+
+// Angular
+import { AngularFireModule } from '@angular/fire/compat';
+
 
 @NgModule({
   declarations: [
@@ -20,14 +30,21 @@ import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
     ButtonComponent,
     LoginComponent,
     HomeComponent,
-    MenuComponent
+    MenuComponent,
+    CrudComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
     MatIconModule,
-    MatProgressSpinnerModule
+    MatProgressSpinnerModule,
+    // provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
+    // provideFirestore(() => getFirestore())
+
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    MatFormFieldModule,
+    MatInputModule
   ],
   providers: [
     provideAnimationsAsync()
@@ -35,3 +52,6 @@ import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+
+//{"projectId":"crud-materias-c0dc3","appId":"1:927437434348:web:6e7981cf6e6988f68924c9","storageBucket":"crud-materias-c0dc3.appspot.com","apiKey":"AIzaSyBEnYyMdP4GgHbecATmSQtWN-fL-OoKck4","authDomain":"crud-materias-c0dc3.firebaseapp.com","messagingSenderId":"927437434348","measurementId":"G-94HF3LEQ7S"}
